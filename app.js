2790732216be57c3e23a52c34bf46ea767b54bf0
Radosław Łuckoś <luckos.rr@hotmail.com>
2021-02-10 13:02:49 +0100

    All logic finished

--- app.js
+++ app.js
@@ -2,34 +2,46 @@
 const baseCurrEl = document.getElementById('base');
 const ammountEl = document.getElementById('ammount');
 const targetCurrEl = document.getElementById('target');
-const submitBtn = document.getElementById('convert-button')
+const submitBtn = document.getElementById('convert-button');
+const resultEl = document.getElementById('result');
 
 //define api key
-const apiKey = 'd7b589eba6b47d2a4fafcf52'
+const apiKey = 'd7b589eba6b47d2a4fafcf52';
 
 //add event for clicking button
-submitBtn.addEventListener('click', ()=>convert());
-
-//convert function
-const convert=()=>{
-    //get params for api 
-    const params = getValues();
-    const [base,ammout,target] = [...params]
-    //construct url
-    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${base}/${target}`;
-    //fetch data from API
-    fetchData(url);
-}
-
-function getValues(){
+submitBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
     const values = [];
-    values.push(baseCurrEl.value);
-    values.push(ammountEl.value);
-    values.push(targetCurrEl.value);
-    return values;
+    collectValues(values);
+    const [baseCurrency, targetCurrency, ammount] = values;
+    fetchData(baseCurrency, targetCurrency)
+        .then(data=>{
+            const conversionRate = data.conversion_rate;
+            const result = convert(ammount, conversionRate).toFixed(2);
+            displayResult(result, targetCurrency);
+        })
+    
+});
+
+async function fetchData(baseCurrency,targetCurrency){
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCurrency}/${targetCurrency}`);
+    const data = await response.json();    
+    return data;
+    
 }
-async function fetchData(url){
-    const response = await fetch(url);
-    const data= await response.json();
-    console.log(data);
+
+function convert(ammount, conversionRate) {
+    console.log(ammount);
+    console.log(conversionRate);
+    return ammount*conversionRate;
+}
+
+function collectValues(values){
+    values.push(baseCurrEl.value)
+    values.push(targetCurrEl.value)
+    values.push(ammountEl.value)
+}
+
+function displayResult(result, currency){
+    resultEl.innerText = `After convertion you will receive ${result} ${currency}.`
 }
